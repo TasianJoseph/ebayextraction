372140a8ba6c43682fc0b06efcadcc16d92ff4ea
TasianJoseph <tasianpjoseph@gmail.com>
2026-03-07 18:22:15 +0000

    plotting out the skeleton functions

--- load.py
+++ load.py
@@ -1,6 +1,19 @@
 import json
 import psycopg2
-from db_creds import user, password
 
-def load_credem
+# loading the database credentials
+def load_credentials(db_creds):
+    with open(db_creds) as credentials:
+        return json.load(credentials)
 
+
+def db_connection():
+    credentials = load_credentials()
+    return psycopg2.connect(credentials)
+
+
+def load_data(transformed_data):
+    connect = db_connection()
+    try:
+        with connect:
+            with connect.cursor() as cursor:
