5f4844f6aaf675aa99a2bf691def0f2253c3fb6f
TasianJoseph <tasianpjoseph@gmail.com>
2026-03-09 22:19:50 +0000

    Restoring finished load.py

--- load.py
+++ load.py
@@ -2,7 +2,9 @@ import json
 import psycopg2
 
 # loading the database credentials
-def load_credentials(db_creds):
+
+
+def load_credentials(db_creds="db_creds.json"):
     with open(db_creds) as credentials:
         return json.load(credentials)
 
@@ -12,14 +14,92 @@ def db_connection():
     return psycopg2.connect(credentials)
 
 
+# inserts brands into the brand table and returns a look-up dictionary to map brand_name and brand_id to use in the
+# load_listings
 def load_brands(transformed_brands):
     connect = db_connection()
     try:
         with connect:
             with connect.cursor() as cursor:
                 insert_data = """
-                INSERT INTO
-                
-                
-                
+                INSERT INTO brands (brand_name, brand_type)
+                VALUES (%(brand_name)s, %(brand_type)s
+                ON CONFLICT (brand_name) DO NOTHING;
                 """
+                cursor.executemany(insert_data, transformed_brands)
+
+                # querying to ask postgres to return the columns brand_ids postgresql assigned to each brand_name to
+                # returning all the relevant rows - returns them as tuples ([1, Balenciaga])
+
+                cursor.exectue("SELECT brand_idm brand_name FROM brands")
+                brand_lookup = {row[1]: row[0] for row in cursor.fetchall()}
+
+        print(f"  Brands loaded: {len(brand_lookup)}")
+        return brand_lookup
+
+    finally:
+        connect.close()
+
+
+# this function uses the load_brands function's brand dictionary from the databases allocated brand_ids to insert the
+# listings into the db listings table
+
+def load_listings(transformed_listings, brand_lookup):
+    connect = db_connection()
+    try:
+        with connect:
+            with connect.cursor() as cursor:
+                for listing in transformed_listings:
+                    brand_name = listing.pop("brand_name")
+                    listing["brand_id"] = brand_lookup[brand_name]
+
+                    insert_query = """
+                        INSERT INTO listings (
+                        brand_id, title, price, currency, condition, url, listing_type, listing_status, snapshot_date,
+                        search_week
+                        )
+                        VALUES (
+                        %(brand_id)s, %(title)s, %(price)s, %(currency)s, %(condition)s, %(url)s, %(listing_type)s,
+                        %(listing_status)s, %(snapshot_date)s, %(search_week)s
+                        );
+                    """
+                    cursor.executemany(insert_query, transformed_listings)
+
+        print(f"  Listings loaded: {len(transformed_listings)}")
+
+    finally:
+        connect.close()
+
+# inserting sold listings into the sales table
+
+
+def load_sales(transformed_sales):
+    if not transformed_sales:
+        print("  No sales data to load — skipping.")
+        return
+
+    connect = db_connection()
+    try:
+        with connect:
+            with connect.cursor() as cursor:
+                insert_query = """
+                                       INSERT INTO sales (
+                                           listing_id, sold_price, sold_currency, sold_date, snapshot_date
+                                       )
+                                       VALUES (
+                                           %(listing_id)s, %(sold_price)s, %(sold_currency)s, %(sold_date)s, 
+                                           %(snapshot_date)s
+                                       )
+                                       ON CONFLICT (sale_id) DO UPDATE SET
+                                           listing_id      = EXCLUDED.listing_id,
+                                           sold_price      = EXCLUDED.sold_price,
+                                           sold_currency   = EXCLUDED.sold_currency,
+                                           sold_date       = EXCLUDED.sold_date,
+                                           snapshot_date   = EXCLUDED.snapshot_date;
+                                   """
+                cursor.executemany(insert_query, transformed_sales)
+
+        print(f"  Sales loaded: {len(transformed_sales)}")
+
+    finally:
+        connect.close()
