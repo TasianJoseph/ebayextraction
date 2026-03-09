372f658d2c26ef72b6810bef3dfc2428d1daceb5
TasianJoseph <tasianpjoseph@gmail.com>
2026-03-09 16:11:31 +0000

    updating load file, switching to specified branch

--- load.py
+++ load.py
@@ -12,8 +12,14 @@ def db_connection():
     return psycopg2.connect(credentials)
 
 
-def load_data(transformed_data):
+def load_brands(transformed_brands):
     connect = db_connection()
     try:
         with connect:
             with connect.cursor() as cursor:
+                insert_data = """
+                INSERT INTO
+                
+                
+                
+                """
